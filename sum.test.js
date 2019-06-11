2bd91b2fce1807e4831c45f587dd00f9e4665bbe
Kishore S <kishoreas7298@gmail.com>
2019-06-11 23:01:36 +0530

    Numbers
    to test greater then less than etc
    
    Signed-off-by: Kishore S <kishoreas7298@gmail.com>

--- sum.test.js
+++ sum.test.js
@@ -1,20 +1,28 @@
 //const sum = require('./sum');
 
 test('adds positive number is not zero',()=>{
-    const n= null;
-    expect(n).toBeNull();
-    expect(n).toBeDefined();
-    expect(n).toBeFalsy();
-    expect(n).not.toBeTruthy();
+    const value = 2 + 2;
+    expect(value).toBeGreaterThan(3);
+    expect(value).toBeGreaterThanOrEqual(3.5);
+    expect(value).toBeLessThan(5);
+    expect(value).toBeLessThanOrEqual(4.5);
 })
 
 /*
     1. toBe (to comapare the values)
     2. toEqual (to compare the Objects)
     3. .not.toBe (opposite of toBe)
+
+    Truthieness
     4.  toBeNull
     5.  toBeDefined
     6.  toBeUndefined
     7.  toBeFalsy
     8.  toBeTruthy
+
+    Numbers
+    1.  toBeGreaterThan
+    2.  toBeGreaterThanOrEqual
+    3.  toBeLessThan
+    4.  toBeLessThanOrEqual
 */
