0d253098eb1375a90baf645b783ff26b20cd2616
Kishore S <kishoreas7298@gmail.com>
2019-06-11 22:47:55 +0530

    Testing over Iteration
    using .not.toBe to see the opposite testing of toBe
    
    Signed-off-by: Kishore S <kishoreas7298@gmail.com>

--- sum.test.js
+++ sum.test.js
@@ -1,5 +1,15 @@
-const sum = require('./sum');
+//const sum = require('./sum');
 
-test('adds 1 and 2 to equal 3',()=>{
-    expect(sum(1,2)).toEqual({a:1,b:2});
+test('adds positive number is not zero',()=>{
+    for(let i=1; i<10; i++){
+        for(let j=1; j<10; j++){
+            expect(i+j).not.toBe(0)
+        }
+    }
 })
+
+/*
+    1. toBe (to comapare the values)
+    2. toEqual (to compare the Objects)
+    3. .not.toBe (opposite of toBe)
+*/
